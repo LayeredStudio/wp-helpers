6280a264d6f87d6ba80199cd75aa0148719c3c09
Andrei Igna <andrei.igna@layered.studio>
2018-09-17 12:26:37 +0200

    Updated - code example for adding Post Type

--- example.php
+++ example.php
@@ -5,47 +5,48 @@ require 'vendor/autoload.php';
 use Layered\Wp\CustomPostType;
 
 
-Layered\Wp\CustomPostType::$i18n = 'my-theme-or-plugin';
+add_action('init', function() {
+
+	// Add a custom post type
+	CustomPostType::add('idea', [
+		'labels'	=>	[
+			'name'	=>	__('Ideas', 'my-theme-or-plugin')
+		],
+		'rewrite'	=>	[
+			'slug'	=>	'my-ideas'
+		],
+		'supports'	=>	['title', 'editor', 'thumbnail', 'excerpt', 'author']
+	])
+		->addTaxonomy('tag')
+	;
 
 
-// Add a custom post type
-CustomPostType::add('idea', [
-	'labels'	=>	[
-		'name'	=>	__('Ideas', 'my-theme-or-plugin')
-	],
-	'rewrite'	=>	[
-		'slug'	=>	'my-ideas'
-	],
-	'supports'	=>	['title', 'editor', 'thumbnail', 'excerpt', 'author']
-])
-	->addTaxonomy('tag')
-	->addColumns(['author'])
-;
+	// Add Recipes - uses Custom Post Type & Meta Fields
+	CustomPostType::add('recipe')
+		->addTaxonomy('difficulty')
+		->addMetaFields([
+			'servings'		=>	[
+				'name'				=>	__('Servings', 'my-theme-or-plugin'),
+				'placeholder'		=>	__('Ex: 1 cake, 2 servings, 5 cupcakes', 'my-theme-or-plugin')
+			],
+			'ingredients'	=>	[
+				'name'				=>	__('Ingredients', 'my-theme-or-plugin'),
+				'placeholder'		=>	__('Ex: 200g quinoa, 2 tbsp olive oil', 'my-theme-or-plugin'),
+				'single'			=>	false
+			],
+			'methods'		=>	[
+				'name'				=>	__('Methods', 'my-theme-or-plugin'),
+				'placeholder'		=>	__('Ex: mix & stir the ingredients', 'my-theme-or-plugin'),
+				'single'			=>	false
+			],
+			'time'			=>	[
+				'type'				=>	'number',
+				'name'				=>	__('Prep time', 'my-theme-or-plugin'),
+				'placeholder'		=>	__('Ex: 15', 'my-theme-or-plugin'),
+				'suffix'			=>	'minutes',
+				'show_in_columns'	=>	true
+			]
+		])
+	;
 
-
-// Add Recipes - uses Custom Post Type & MetaBox
-CustomPostType::add('recipe')
-	->addTaxonomy('difficulty')
-	->addColumns(['author'])
-	->addMetaBox('Recipe Info', [[
-		'id'			=>	'servings',
-		'title'			=>	__('Servings', 'my-theme-or-plugin'),
-		'placeholder'	=>	__('Ex: 1 cake, 2 servings, 5 cupcakes', 'my-theme-or-plugin')
-	], [
-		'id'			=>	'ingredients',
-		'title'			=>	__('Ingredients', 'my-theme-or-plugin'),
-		'placeholder'	=>	__('Ex: 200g quinoa, 2 tbsp olive oil', 'my-theme-or-plugin'),
-		'multiple'		=>	true
-	], [
-		'id'			=>	'methods',
-		'title'			=>	__('Methods', 'my-theme-or-plugin'),
-		'placeholder'	=>	__('Ex: mix & stir the ingredients', 'my-theme-or-plugin'),
-		'multiple'		=>	true
-	], [
-		'id'			=>	'time',
-		'type'			=>	'number',
-		'title'			=>	__('Prep time', 'my-theme-or-plugin'),
-		'placeholder'	=>	__('Ex: 15', 'my-theme-or-plugin'),
-		'suffix'		=>	'minutes'
-	]])
-;
+});
